791e1c97624295fee5d5a3d6c7e678d187a2d19e
Yau Ref <yau.ref@gmail.com>
2014-11-25 02:21:56 +0300

    Created simple test: not empty with 3 elements

--- src/tests/scala/Tests.scala
+++ src/tests/scala/Tests.scala
@@ -1,16 +1,21 @@
 package tests
 
 import org.scalatest.FunSuite
+import scala.util.Try
 import esox._
+import esox.termops.{GetLength, TerminalOperation}
 
 class Tests extends FunSuite{
 
-  val simpleList = List(1,2,3,2,1)
+  val simpleList = List(1,2,3,4,5)
+  implicit val performer = new Performer {
+    override def performSafe[A, B](op: TerminalOperation[A, B]): Try[B] = Try(op.result)
+  }
 
-  test("Common test"){
-    simpleList.rem.filter(x => x > 0)
-
-
+  test("Not empty with 2 elements"){
+    val collection = simpleList.rem.filter(_ > 2).map(_ + "!")
+    assert(collection.isEmpty === false)
+    assert(collection.length === 3)
   }
 
 }

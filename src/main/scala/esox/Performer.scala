505b9b23589dcd024abc60ddd468e2e540e1b621
Yau Ref <yau.ref@gmail.com>
2014-11-26 01:27:22 +0300

    Using ClassTag for terminal operations and Performer

--- src/main/scala/esox/Performer.scala
+++ src/main/scala/esox/Performer.scala
@@ -1,13 +1,15 @@
 package esox
 
 import esox.termops.TerminalOperation
+
+import scala.reflect.ClassTag
 import scala.util.{Failure, Success, Try}
 
 trait Performer {
 
-  def performSafe[A, B](op: TerminalOperation[A, B]): Try[B]
+  def performSafe[A: ClassTag, B: ClassTag](op: TerminalOperation[A, B]): Try[B]
 
-  def perform[A, B](op: TerminalOperation[A, B]) = performSafe(op) match {
+  def perform[A: ClassTag, B: ClassTag](op: TerminalOperation[A, B]) = performSafe(op) match {
     case Success(res) => res
     case Failure(e) => throw e
   }
--- src/main/scala/esox/termops/package.scala
+++ src/main/scala/esox/termops/package.scala
@@ -1,37 +1,39 @@
 package esox
 
+import scala.reflect.ClassTag
+
 package object termops {
 
-  sealed trait TerminalOperation[A, B] {
+  abstract class TerminalOperation[A: ClassTag, B: ClassTag] {
     val collection: RemoteCollection[A]
     def result: B
   }
 
-  case class GetLength[A](collection: RemoteCollection[A]) extends TerminalOperation[A, Int] {
+  case class GetLength[A: ClassTag](collection: RemoteCollection[A]) extends TerminalOperation[A, Int] {
     def result = collection.data.size
   }
 
-  case class Count[A](collection: RemoteCollection[A], p: A => Boolean) extends TerminalOperation[A, Int] {
+  case class Count[A: ClassTag](collection: RemoteCollection[A], p: A => Boolean) extends TerminalOperation[A, Int] {
     def result = collection.data.count(p)
   }
 
-  case class Exists[A](collection: RemoteCollection[A], p: A => Boolean) extends TerminalOperation[A, Boolean] {
+  case class Exists[A: ClassTag](collection: RemoteCollection[A], p: A => Boolean) extends TerminalOperation[A, Boolean] {
     def result = collection.data.exists(p)
   }
 
-  case class IsEmpty[A](collection: RemoteCollection[A]) extends TerminalOperation[A, Boolean] {
+  case class IsEmpty[A: ClassTag](collection: RemoteCollection[A]) extends TerminalOperation[A, Boolean] {
     def result = collection.data.isEmpty
   }
 
-  case class Reduce[A, B >: A](collection: RemoteCollection[A], f: (B, A) => B) extends TerminalOperation[A, B] {
+  case class Reduce[A: ClassTag, B >: A : ClassTag](collection: RemoteCollection[A], f: (B, A) => B) extends TerminalOperation[A, B] {
     def result = collection.data.reduceLeft(f)
   }
 
-  case class Find[A](collection: RemoteCollection[A], p: A => Boolean) extends TerminalOperation[A, Option[A]] {
+  case class Find[A: ClassTag](collection: RemoteCollection[A], p: A => Boolean) extends TerminalOperation[A, Option[A]] {
     def result = collection.data.find(p)
   }
 
-  case class GetBack[A](collection: RemoteCollection[A]) extends TerminalOperation[A, Traversable[A]] {
+  case class GetBack[A: ClassTag](collection: RemoteCollection[A]) extends TerminalOperation[A, Traversable[A]] {
     def result = collection.data
   }
 

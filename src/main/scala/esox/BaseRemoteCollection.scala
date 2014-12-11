c62364dfd1e1b731d29e8d19b7d6ab7c472e89c9
Yau Ref <yau.ref@gmail.com>
2014-12-12 01:27:18 +0300

    Performer argument of BaseRemoteCollection is not implicit anymore

--- src/main/scala/esox/BaseRemoteCollection.scala
+++ src/main/scala/esox/BaseRemoteCollection.scala
@@ -3,8 +3,9 @@ package esox
 import scala.collection.Traversable
 import scala.reflect.ClassTag
 
-class BaseRemoteCollection[A: ClassTag](val localCollection: Traversable[A])
-                                       (override implicit val performer: Performer) extends RemoteCollection[A] {
+
+class BaseRemoteCollection[A: ClassTag](val localCollection: Traversable[A],
+                                        override val performer: Performer) extends RemoteCollection[A] {
 
   override def data: Traversable[A] = localCollection
 
